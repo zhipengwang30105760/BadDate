d1e067e5b9055191e5aec432558e84a3b1de2f55
zhipeng <zhipengwang@unomaha.edu>
2019-06-05 10:57:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/application/baddate/NotificationHelper.java
@@ -0,0 +1,52 @@
+package com.example.application.baddate;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+import com.example.application.baddate.R;
+
+
+public class NotificationHelper extends ContextWrapper {
+    public static final String channelID = "channelID";
+    public static final String channelName = "Channel Name";
+
+    private NotificationManager mManager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        channel.enableLights(true);
+        channel.enableVibration(true);
+        channel.setLightColor(R.color.colorPrimary);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if (mManager == null) {
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return mManager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification() {
+        return new NotificationCompat.Builder(getApplicationContext(), channelID)
+                .setContentTitle("Message!")
+                .setContentText("Your mom send you a message.")
+                .setSmallIcon(R.drawable.ic_message);
+    }
+}
