f5b65e5a8b655072be7b087cb69a2f1dac5280bd
zhipeng <zhipengwang@unomaha.edu>
2019-06-19 18:14:37 -0500

    add phone call function

--- app/src/main/java/com/example/application/baddate/AlertReceiver.java
+++ app/src/main/java/com/example/application/baddate/AlertReceiver.java
@@ -9,8 +9,11 @@ import android.support.v4.app.NotificationCompat;
 public class AlertReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+
         NotificationHelper notificationHelper = new NotificationHelper(context);
         NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
         notificationHelper.getManager().notify(1, nb.build());
+
+
     }
 }
--- app/src/main/java/com/example/application/baddate/MainActivity.java
+++ app/src/main/java/com/example/application/baddate/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.application.baddate;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.application.baddate.R;
 
@@ -20,7 +27,7 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     private TextView mTextView;
-
+    private static final int REQUEST_CALL = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,16 +72,29 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     }
 
     private void startAlarm(Calendar c) {
+
+
+
+        // Send notification
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlertReceiver.class);
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        //Intent intent = new Intent(this, AlertReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+        intent.setData(Uri.parse("tel:12312312"));//change the number
 
         if (c.before(Calendar.getInstance())) {
             c.add(Calendar.DATE, 1);
         }
-
-
+        startActivity(intent);
+        // phone call function works but I cannot schedule the phone call when the time exactly match the instance time.
+        /*
+        if (c.compareTo((Calendar) Calendar.getInstance()) != 0) {
+            makePhoneCall();
+        }
+        */
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+
+
     }
 
     private void cancelAlarm() {
@@ -85,4 +105,29 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         alarmManager.cancel(pendingIntent);
         mTextView.setText("Canceled");
     }
+    /*
+    private void makePhoneCall() {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            String dial = "tel:1234567";
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+``*/
+
 }
